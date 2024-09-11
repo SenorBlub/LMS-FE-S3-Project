215dcabb7c3b75c28b9359bc033838ce218eef47
SenorBlub <thomasverhappen@Gmail.com>
2024-09-11 16:04:14 +0200

    small styling update

--- src/components/TheLogin.vue
+++ src/components/TheLogin.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-  <div class="container">
-    <h1>LOG IN</h1>
+  <div class="container grey-lightest-bg round-edges">
+    <h1 class="text-bigger grey-darkest-text">LOG IN</h1>
     <input type="text" class="text-medium grey-dark-text round-edges" />
     <input />
   </div>
