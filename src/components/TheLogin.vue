ea33372b52e7798948cedb3c952cf6db29837694
SenorBlub <thomasverhappen@Gmail.com>
2024-09-11 16:45:11 +0200

    structuring tweak

--- src/components/TheLogin.vue
+++ src/components/TheLogin.vue
@@ -3,26 +3,28 @@
 </script>
 
 <template>
-  <div class="container grey-lightest-bg round-edges">
-    <h1 class="text-bigger grey-darkest-text">LOG IN</h1>
-    <input
-      type="text"
-      class="login-input grey-light-bg text-medium placeholder-text grey-darkest-text round-edges shadow-out no-border"
-      id="login-username"
-      placeholder="Username"
-    />
-    <input
-      type="password"
-      class="login-input grey-light-bg text-medium placeholder-text grey-darkest-text round-edges shadow-out no-border"
-      id="login-password"
-      placeholder="Password"
-    />
+  <div>
+    <div class="container grey-lightest-bg round-edges">
+      <h1 class="text-bigger grey-darkest-text">LOG IN</h1>
+      <input
+        type="text"
+        class="login-input grey-light-bg text-medium placeholder-text grey-darkest-text round-edges shadow-out no-border"
+        id="login-username"
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        class="login-input grey-light-bg text-medium placeholder-text grey-darkest-text round-edges shadow-out no-border"
+        id="login-password"
+        placeholder="Password"
+      />
+    </div>
+    <button
+      class="login-button purple-main-bg text-medium grey-darkest-text round-edges shadow-out no-border"
+    >
+      Log In
+    </button>
   </div>
-  <button
-    class="login-button purple-main-bg text-medium grey-darkest-text round-edges shadow-out no-border"
-  >
-    Log In
-  </button>
 </template>
 
 <style scoped>
