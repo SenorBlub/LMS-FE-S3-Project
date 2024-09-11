91e3d1cc231765feab51a6b85aa4848306eef6f7
SenorBlub <thomasverhappen@Gmail.com>
2024-09-11 11:29:17 +0200

    automatic deploy attempt 3

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You updated it! Visual clue" />
+      <HelloWorld msg="You updated it! Visual clue again" />
     </div>
   </header>
 
