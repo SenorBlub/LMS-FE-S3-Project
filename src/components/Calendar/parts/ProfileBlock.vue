8c124d601967afac0e11759ee5c02eb5a9fc4afb
SenorBlub <thomasverhappen@Gmail.com>
2024-09-17 09:11:52 +0200

    new div :3

--- src/components/Calendar/parts/ProfileBlock.vue
+++ src/components/Calendar/parts/ProfileBlock.vue
@@ -3,13 +3,13 @@
 <template>
   <div class="wrapper-side grey-light-bg text-center grey-darkest-text text-small round-edges">
     <div class="profile-picture grey-lightest-bg grey-darkest-text text-small round-full">PFP</div>
-    USERNAME
+    <div>USERNAME</div>
   </div>
 </template>
 
 <style scoped>
 .profile-picture {
-  height: 140px;
-  width: 140px;
+  height: 100px;
+  width: 100px;
 }
 </style>
