08817f4a6f0b1aa774a342a40340edc02cb1c8dc
SenorBlub <thomasverhappen@Gmail.com>
2024-09-17 08:59:43 +0200

    small styling changes ProfileBlock.vue

--- src/components/Calendar/parts/ProfileBlock.vue
+++ src/components/Calendar/parts/ProfileBlock.vue
@@ -2,9 +2,14 @@
 
 <template>
   <div class="wrapper-side grey-light-bg text-center grey-darkest-text text-small round-edges">
-    <div class="profile-picture grey-darkest-text text-small">PFP</div>
+    <div class="profile-picture grey-lightest-bg grey-darkest-text text-small round-full">PFP</div>
     USERNAME
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.profile-picture {
+  height: 140px;
+  width: 140px;
+}
+</style>
